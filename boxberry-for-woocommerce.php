26514560c468fae0f3c8a9ac0eb4bcb8d14138ea
Sanya <sanya.kuzovlev@gmail.com>
2017-04-04 13:39:46 +0300

    Initial commit

---
+++ boxberry-for-woocommerce.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Plugin Name: Boxberry for Woocommerce
+ * Plugin URI: https://github.com/hdi-io/boxberry-for-woocommerce
+ * Description: Boxberry delivery service integration for Woocommerce.
+ * Version: 0.0.1
+ * Author: HDI
+ * Author URI: http://hdi.io/
+ * Text Domain: boxberry-for-woocommerce
+ * Domain Path: /languages/
+ *
+ * Users Profile Navigation is distributed under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Users Profile Navigation is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Users Profile Navigation. If not, see <http://www.gnu.org/licenses/>.
+ */
